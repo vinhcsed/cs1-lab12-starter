e700df0de2a609f6c9230a82d29fffeed6aa323f
Vincent Tran <33761211+VincentVinhTran@users.noreply.github.com>
2025-05-06 11:58:29 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "soda.h"
+using namespace std;
+
+/***** Function Prototypes (Optional) *****/
+/* Reads from inventory.txt and retrieves the number of soda varieties;
+   takes an input filestream as an argument;
+   returns the number of soda varieties */
+// int getNumberOfSodas(fstream&);
+
+/* Reads from inventory.txt and dynamically allocates an array of Soda;
+   takes an input filestream and the number of soda varieties as arguments;
+   returns the initialized array */
+// Soda* readInventory(fstream&, int);
+
+/* Displays choices in required format;
+   takes the Soda array and its size as arguments */
+// void displayChoices(Soda*, int);
+
+/* Gets user choice and continuously prompts until valid; 
+   takes the size of the Soda array as an argument;
+   returns the selection */
+// int getSelection(int);
+
+/* Gets user payment and continuously prompts until valid;
+   takes the Soda array and a selection number as arguments;
+   modifies the array and returns the appropriate amount of change */
+// double purchase(Soda*, int);
+
+/* Writes the remaining inventory to inventory.txt;
+   takes an output filestream, the Soda array, and its size as arguments */
+// void writeInventory(fstream&, Soda*, int);
+
+int main() { 
+  return 0;
+}
---
+++ soda.h
@@ -0,0 +1,8 @@
+// soda.h
+
+#ifndef SODA_H
+#define SODA_H
+
+// Define Soda structure here
+
+#endif // SODA_H
